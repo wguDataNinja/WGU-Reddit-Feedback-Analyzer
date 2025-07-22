4c2d2de367f0434e58f8df28cfacb61982b3f886
wguDataNinja <jowe160@wgu.edu>
2025-07-22 03:27:02 -0400

    Remove db, dev, and selected fetcher scripts from tracking

--- fetchers/helpers.py
+++
@@ -1,9 +0,0 @@
-# fetchers/helpers.py
-
-from utils.paths import project_path
-import pandas as pd
-
-def load_tracked_subreddits():
-    csv_path = project_path("data", "wgu_subreddits.csv")
-    df = pd.read_csv(csv_path)
-    return df.iloc[:, 0].dropna().tolist()
